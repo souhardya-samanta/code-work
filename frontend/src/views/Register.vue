b0e1318eb42778b74a8c2dcf708812a0775e95c8
souhardya69 <samantasouhardya@hotmail.com>
2021-06-25 13:51:10 +0530

    register page

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,5 +1,156 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <main>
+      <section class="absolute w-full h-full">
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="
+                  relative
+                  flex flex-col
+                  min-w-0
+                  break-words
+                  w-full
+                  mb-6
+                  shadow-lg
+                  rounded-lg
+                  bg-gray-300
+                  border-0
+                "
+              >
+                <div class="rounded-t mb-0 px-6 py-6">
+                  <div class="text-center mb-3">
+                    <h6 class="text-gray-600 text-l font-bold">
+                      Let's get started!
+                    </h6>
+                  </div>
+                </div>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <form>
+                     <div class="w-full mb-2">
+                      <input
+                        type="name"
+                        v-model="name"
+                        class="
+                          border-0
+                          px-3
+                          py-3
+                          placeholder-gray-400
+                          text-gray-700
+                          bg-white
+                          rounded
+                          text-sm
+                          shadow
+                          focus:outline-none
+                          focus:ring
+                          w-full
+                        "
+                        placeholder="Name"
+                        style="transition: all 0.15s ease 0s"
+                      />
+                    </div>
+                    <div class="w-full mb-2">
+                      <input
+                        type="number"
+                        v-model="age"
+                        class="
+                          border-0
+                          px-3
+                          py-3
+                          placeholder-gray-400
+                          text-gray-700
+                          bg-white
+                          rounded
+                          text-sm
+                          shadow
+                          focus:outline-none
+                          focus:ring
+                          w-full
+                        "
+                        placeholder="Age"
+                        style="transition: all 0.15s ease 0s"
+                      />
+                    </div>
+                    <div class="w-full mb-2">
+                      <input
+                        type="email"
+                        v-model="email"
+                        class="
+                          border-0
+                          px-3
+                          py-3
+                          placeholder-gray-400
+                          text-gray-700
+                          bg-white
+                          rounded
+                          text-sm
+                          shadow
+                          focus:outline-none
+                          focus:ring
+                          w-full
+                        "
+                        placeholder="Email"
+                        style="transition: all 0.15s ease 0s"
+                      />
+                    </div>
+                    <div class="w-full mb-2">
+                      <input
+                        type="password"
+                        class="
+                          border-0
+                          px-3
+                          py-3
+                          placeholder-gray-400
+                          text-gray-700
+                          bg-white
+                          rounded
+                          text-sm
+                          shadow
+                          focus:outline-none
+                          focus:ring
+                          w-full
+                        "
+                        placeholder="Password"
+                        style="transition: all 0.15s ease 0s"
+                      />
+                    </div>
+                    <div class="text-center mt-6">
+                      <button
+                        class="
+                          bg-green-500
+                          text-white
+                          active:bg-gray-700
+                          text-sm
+                          font-bold
+                          uppercase
+                          px-6
+                          py-3
+                          rounded
+                          shadow
+                          hover:shadow-lg
+                          outline-none
+                          focus:outline-none
+                          mr-1
+                          mb-1
+                          w-full
+                        "
+                        type="button"
+                        style="transition: all 0.15s ease 0s"
+                      >
+                        SIGNUP
+                      </button>
+                    </div>
+                  </form>
+                   <div class="text-right">
+                    <router-link to="/login"><small>Already have an account? Login instead</small></router-link>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
