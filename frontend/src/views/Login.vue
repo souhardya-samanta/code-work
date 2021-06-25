03537ec9f683346852d19f238e91d4a4a17132f0
souhardya69 <samantasouhardya@hotmail.com>
2021-06-25 13:34:07 +0530

    login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <main>
+      <section class="absolute w-full h-full">
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="
+                  relative
+                  flex flex-col
+                  min-w-0
+                  break-words
+                  w-full
+                  mb-6
+                  shadow-lg
+                  rounded-lg
+                  bg-gray-300
+                  border-0
+                "
+              >
+                <div class="rounded-t mb-0 px-6 py-6">
+                  <div class="text-center mb-3">
+                    <h6 class="text-gray-600 text-l font-bold">
+                      Welcome Back!!
+                    </h6>
+                  </div>
+                </div>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <form>
+                    <div class="w-full mb-2">
+                      <input
+                        type="email"
+                        class="
+                          border-0
+                          px-3
+                          py-3
+                          placeholder-gray-400
+                          text-gray-700
+                          bg-white
+                          rounded
+                          text-sm
+                          shadow
+                          focus:outline-none
+                          focus:ring
+                          w-full
+                        "
+                        placeholder="Email"
+                        style="transition: all 0.15s ease 0s"
+                      />
+                    </div>
+                    <div class="w-full mb-2">
+                      <input
+                        type="password"
+                        class="
+                          border-0
+                          px-3
+                          py-3
+                          placeholder-gray-400
+                          text-gray-700
+                          bg-white
+                          rounded
+                          text-sm
+                          shadow
+                          focus:outline-none
+                          focus:ring
+                          w-full
+                        "
+                        placeholder="Password"
+                        style="transition: all 0.15s ease 0s"
+                      />
+                    </div>
+                    <div class="text-center mt-6">
+                      <button
+                        class="
+                          bg-green-500
+                          text-white
+                          active:bg-gray-700
+                          text-sm
+                          font-bold
+                          uppercase
+                          px-6
+                          py-3
+                          rounded
+                          shadow
+                          hover:shadow-lg
+                          outline-none
+                          focus:outline-none
+                          mr-1
+                          mb-1
+                          w-full
+                        "
+                        type="button"
+                        style="transition: all 0.15s ease 0s"
+                      >
+                        LOGIN
+                      </button>
+                    </div>
+                  </form>
+                   <div class="text-right">
+                    <router-link to="/"><small>New here? Create an account!</small></router-link>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<style>
+.chat-notification {
+  display: flex;
+  max-width: 24rem;
+  margin: 0 auto;
+  padding: 1.5rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
+}
+.chat-notification-logo-wrapper {
+  flex-shrink: 0;
+}
+.chat-notification-logo {
+  height: 3rem;
+  width: 3rem;
+}
+.chat-notification-content {
+  margin-left: 1.5rem;
+  padding-top: 0.25rem;
+}
+.chat-notification-title {
+  color: #1a202c;
+  font-size: 1.25rem;
+  line-height: 1.25;
+}
+.chat-notification-message {
+  color: #718096;
+  font-size: 1rem;
+  line-height: 1.5;
+}
+</style>
